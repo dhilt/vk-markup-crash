9715d0f8dec4a83d514ce044afece196a4df2394
dhilt <d.alexanov@gmail.com>
2019-02-10 14:33:55 +0300

    rules.js

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,92 +1,91 @@
+(() => {
 
-const LSToken = 'vk_markup_crash_options';
+  const LSToken = 'vk_markup_crash_options'
 
-const RULES = {
-	ADDS: `
-._ads_block_data_w, .ads_left, .ads_ads_box {
-	display: none !important;
-}`,
-};
-
-const vk_markup_crash = {
+  const vk_markup_crash = {
 
     getStorageDataAsync: function (callback) {
-    	return new Promise(resolve => 
-            chrome.storage.local.get(LSToken, (items) => {
-	            const storageOptions = items[LSToken];
-	            if (!storageOptions) {
-	                return;
-	            }
-	            resolve(storageOptions);
-	        })
-    	);
+      return new Promise(resolve =>
+        chrome.storage.local.get(LSToken, (items) => {
+          const storageOptions = items[LSToken]
+          if (!storageOptions) {
+            return
+          }
+          resolve(storageOptions)
+        })
+      )
     },
 
-    processStorageOptions: function(options) {
+    processStorageOptions: function (options) {
       if (options) {
-      	if (this.getOption(options, "ads")) {
-			this.addCss(RULES.ADDS);
-      	}
+        if (this.getOption(options, 'ads')) {
+          this.addCss(RULES.ADDS)
+        }
+        if (this.getOption(options, 'round_icons')) {
+          this.addCss(RULES.ROUND_ICONS)
+        }
       }
     },
 
-	getOption: function (options, token) {
-      	const option = options.find(option => option.id === token);
-      	return option && option.value;
-	},
+    getOption: function (options, token) {
+      const option = options.find(option => option.id === token)
+      return option && option.value
+    },
 
-	addCss: function (rule) {
-	  const css = document.createElement('style');
-	  css.type = 'text/css';
-	  css.appendChild(document.createTextNode(rule));
-	  (document.head || document.documentElement).appendChild(css);
-	},
+    addCss: function (rule) {
+      const css = document.createElement('style')
+      css.type = 'text/css'
+      css.appendChild(document.createTextNode(rule));
+      (document.head || document.documentElement).appendChild(css)
+    },
 
-	injectStyle: function () {
-		var style = document.createElement("link");
-		style.rel = "stylesheet";
-		style.type = "text/css";
-		style.href = chrome.extension.getURL("replacement.css");
-		(document.head || document.documentElement).appendChild(style);
+    injectStyle: function () {
+      // base styles
+      var style = document.createElement('link')
+      style.rel = 'stylesheet'
+      style.type = 'text/css'
+      style.href = chrome.extension.getURL('replacement.css');
+      (document.head || document.documentElement).appendChild(style)
 
-		this.getStorageDataAsync().then(options => 
-			this.processStorageOptions(options)
-		);
-	},
+      // optioned styles
+      this.getStorageDataAsync().then(
+        this.processStorageOptions.bind(this)
+      )
+    },
 
-	isHeader: true,
+    isHeader: true,
 
-	processHeader: function (header) {
-		var self = this;
-		window.onscroll = function() {
-			var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-			if(self.isHeader && scrolled > 0) {
-				self.isHeader = false;
-				header.style.display = 'none';
-			}
-			else if (!self.isHeader && scrolled === 0) {
-				self.isHeader = true;
-				header.style.display = 'block';	
-			}
-		}
-	},
+    processHeader: function (header) {
+      var self = this
+      window.onscroll = function () {
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop
+        if (self.isHeader && scrolled > 0) {
+          self.isHeader = false
+          header.style.display = 'none'
+        }
+        else if (!self.isHeader && scrolled === 0) {
+          self.isHeader = true
+          header.style.display = 'block'
+        }
+      }
+    },
 
-	processDOM: function () {
-		var self = this;
-		self.timerId = setInterval(function () {
-			var header = document.getElementById('page_header_cont');
-			if (header) {
-				clearInterval(self.timerId);
-				self.processHeader(header);
-			}
-		}, 75);
-	},
+    processDOM: function () {
+      this.timerId = setInterval(() => {
+        var header = document.getElementById('page_header_cont')
+        if (header) {
+          clearInterval(this.timerId)
+          this.processHeader(header)
+        }
+      }, 75)
+    },
 
-	run: function () {
-		this.injectStyle();
-		this.processDOM();
-	}
-};
+    run: function () {
+      this.injectStyle()
+      this.processDOM()
+    }
+  }
 
+  vk_markup_crash.run()
 
-vk_markup_crash.run();
+})()
---
+++ src/rules.js
@@ -0,0 +1,39 @@
+const RULES = {
+    ADDS: `
+._ads_block_data_w, .ads_left, .ads_ads_box {
+    display: none !important;
+}`,
+    ROUND_ICONS: `
+.top_profile_img,
+.chat_tab_img,
+.module_body .people_cell_img, 
+.post_img, 
+.wall_module .copy_post_img, 
+.page_list_module .thumb, 
+.page_list_module .cell_img,
+.wall_module .reply_img, 
+.post_field_user_image,
+.module_body .people_cell_img,
+.page_list_module .thumb,
+.ow_ava.ow_ava_comm,
+.friends_photo_img,
+.right_list_img,
+.ow_ava.ow_ava_small,
+.mv_author_photo,
+.group_row_photo, .group_row_img,
+.feedback_img,
+.nim-peer .nim-peer--photo-w,
+.feed_friend_image,
+.feed_friend_photo_wrap .ui_zoom_inner, 
+.nim-peer.nim-peer_small .im_grid>img,
+.nim-peer .nim-peer--photo .im_grid>img, 
+.nim-peer .nim-peer--photo>img,
+.nim-dialog.nim-dialog_classic .nim-dialog--photo .nim-peer .nim-peer--photo .im_grid>img,
+.like_tt_image, .poll_tt_image,
+.like_share_ava.wdd_imgs,
+.wddi_img,
+.submit_post_box .thumb_off, .submit_post_box .thumb_on
+{
+    border-radius: 0 !important;
+}`
+};
