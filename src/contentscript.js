0987d597bd84d76ff01327490ab7b3372e6ce434
dhilt <d.alexanov@gmail.com>
2019-02-10 14:16:50 +0300

    options

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,4 +1,46 @@
-var vk_markup_crash = {
+
+const LSToken = 'vk_markup_crash_options';
+
+const RULES = {
+	ADDS: `
+._ads_block_data_w, .ads_left, .ads_ads_box {
+	display: none !important;
+}`,
+};
+
+const vk_markup_crash = {
+
+    getStorageDataAsync: function (callback) {
+    	return new Promise(resolve => 
+            chrome.storage.local.get(LSToken, (items) => {
+	            const storageOptions = items[LSToken];
+	            if (!storageOptions) {
+	                return;
+	            }
+	            resolve(storageOptions);
+	        })
+    	);
+    },
+
+    processStorageOptions: function(options) {
+      if (options) {
+      	if (this.getOption(options, "ads")) {
+			this.addCss(RULES.ADDS);
+      	}
+      }
+    },
+
+	getOption: function (options, token) {
+      	const option = options.find(option => option.id === token);
+      	return option && option.value;
+	},
+
+	addCss: function (rule) {
+	  const css = document.createElement('style');
+	  css.type = 'text/css';
+	  css.appendChild(document.createTextNode(rule));
+	  (document.head || document.documentElement).appendChild(css);
+	},
 
 	injectStyle: function () {
 		var style = document.createElement("link");
@@ -6,6 +48,10 @@ var vk_markup_crash = {
 		style.type = "text/css";
 		style.href = chrome.extension.getURL("replacement.css");
 		(document.head || document.documentElement).appendChild(style);
+
+		this.getStorageDataAsync().then(options => 
+			this.processStorageOptions(options)
+		);
 	},
 
 	isHeader: true,
---
+++ src/options.js
@@ -0,0 +1,88 @@
+(() => {
+
+  const LS = localStorage
+  const LSToken = 'vk_markup_crash_options'
+
+  const vk_markup_crash_options = {
+
+    options: [{
+      id: 'ads',
+      type: 'checkbox',
+      value: true,
+      text: 'remove ads'
+    }],
+
+    saveOptions: function () {
+      LS[LSToken] = JSON.stringify(this.options)
+      chrome.storage.local.set({
+        [LSToken]: this.options
+      })
+    },
+
+    initializeOptionList: function () {
+      let ls
+      if (LS[LSToken]) {
+        ls = JSON.parse(LS[LSToken])
+      }
+      if (ls && ls.length) {
+        ls.forEach(opt => {
+          const option = this.options.find(_opt => opt.id === _opt.id)
+          if (option) {
+            option.value = opt.value
+          }
+        })
+      }
+    },
+
+    makeCheckbox: function (option) {
+      return `
+    	<input type="checkbox" value="1"
+    		${option.value ? 'checked' : ''} 
+    		name="${option.id}"
+    		id="${option.id}" />
+    	<span> - ${option.text}</span>`
+    },
+
+    makeInput: function (option) {
+      switch (option.type) {
+        default:
+          return this.makeCheckbox(option)
+      }
+    },
+
+    setOptionsHTML: function () {
+      const optionsElement = document.getElementById('options')
+      if (!optionsElement) {
+        return
+      }
+      optionsElement.innerHTML =
+        this.options.map(option => `<div>` + this.makeInput(option) + `</div>`).join('')
+    },
+
+    setOptionsListeners: function () {
+      this.options.forEach(option => {
+        const element = document.getElementById(option.id)
+        if (!element) {
+          console.warn(`vk_markup_crash: options element not found (${option.id})`)
+          return
+        }
+        element.addEventListener('click', () => {
+          if (option.type === 'checkbox') {
+            option.value = !option.value
+          }
+          this.saveOptions()
+        })
+      })
+    },
+
+    run: function () {
+      this.initializeOptionList()
+      this.setOptionsHTML()
+      this.setOptionsListeners()
+    }
+
+  }
+
+  vk_markup_crash_options.run()
+
+})()
