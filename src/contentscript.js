b0075512ffa142b3adf59a4f33795657718be400
Denis Hilt <d.alexanov@gmail.com>
2020-10-14 01:34:54 +0300

    contents: stories, clips & dynamic width

--- src/contentscript.js
+++ src/contentscript.js
@@ -17,43 +17,62 @@
     }
 
     processStorageOptions(options) {
-      if (this.getOption(options, 'HEADER')) {
+      const getOption = (token, subtoken = '') =>
+        this.getOption({ options, settings: this.settings, token, subtoken })
+      if (getOption('HEADER')) {
         this.processHeader()
       }
-      if (this.getOption(options, 'ADS')) {
-        this.addCss(this.rules.ADDS)
+      if (getOption('ADS')) {
+        this.addCss(this.rules.ADS)
       }
-      if (this.getOption(options, 'ROUND_ICONS')) {
+      if (getOption('ROUND_ICONS')) {
         this.addCss(this.rules.ROUND_ICONS)
       }
-      if (this.getOption(options, 'WIDE_LAYOUT')) {
-        this.addCss(this.rules.WIDE_LAYOUT)
+      if (getOption('WIDER_LAYOUT', 'ENABLED')) {
+        const value = getOption('WIDER_LAYOUT', 'WIDTH');
+        this.addCss(this.rules['WIDER_LAYOUT_' + value.toUpperCase()])
       }
-      if (this.getOption(options, 'MARGINS_AND_PADDINGS')) {
+      if (getOption('MARGINS_AND_PADDINGS')) {
         this.addCss(this.rules.MARGINS_AND_PADDINGS)
       }
-      if (this.getOption(options, 'NEW_COMMENT_ICON')) {
+      if (getOption('NEW_COMMENT_ICON')) {
         this.addCss(this.rules.NEW_COMMENT_ICON)
+      }
+      if (getOption('STORIES_CLIPS_RECOMMENDATIONS')) {
+        this.addCss(this.rules.STORIES_CLIPS_RECOMMENDATIONS)
       }
     }
 
-    getOption(options, token) {
+    getOption({ options, settings, token, subtoken }) {
       const option = options.find(option => option.id === token)
-      if (option && typeof option.value === 'boolean') {
+      if (subtoken) {
+        if (option && option.type === 'group') {
+          const sGroup = settings.find(({ id }) => id === option.id);
+          return this.getOption({ options: option.value, settings: sGroup && sGroup.value, token: subtoken })
+        }
+        return false
+      }
+
+      if (option && typeof option.value !== 'undefined') {
         return option.value
       }
-      const defaultOpt = this.settings.find(setting => setting.id === token)
-      if (defaultOpt && typeof defaultOpt.value === 'boolean') {
-        return defaultOpt.value
+      if (Array.isArray(settings)) {
+        const defaultOpt = settings.find(setting => setting.id === token)
+        if (defaultOpt && typeof defaultOpt.value !== 'undefined') {
+          return defaultOpt.value
+        }
       }
       return false
     }
 
     addCss(rule) {
+      if (!rule) {
+        return
+      }
       const css = document.createElement('style')
-      css.type = 'text/css'
-      css.appendChild(document.createTextNode(rule));
-      (document.head || document.documentElement).appendChild(css)
+      const elementToAppend = document.head || document.documentElement
+      css.appendChild(document.createTextNode(rule))
+      elementToAppend.appendChild(css)
     }
 
     processHeader() {
